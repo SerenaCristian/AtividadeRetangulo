e37e81d6c63567a81e1cde22694cdbbe386d1983
Cristian Serena <devcristian02@gmail.com>
2024-03-02 22:00:52 -0300

    Atividade Retangulo - criando a classe e criando o objeto

---
+++ RetanguloSemClasse/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace RetanguloSemClasse
+{
+    public class Retangulo
+    {
+        public double area;
+        public double perimetro;
+        public double altura;
+        public double largura;
+        public double diagonal;
+
+
+        public double CalculaPerimetro(double altura, double largura)
+        {
+            double perimetro = 2 * (altura + largura);
+            return perimetro;
+
+        }
+
+        public  double CalcularArea(double altura, double largura)
+        {
+            double area = altura * largura;
+            return area;
+        }
+        public  double CalcularDiagonal(double altura, double largura)
+        {
+            double diagonal = Math.Sqrt(Math.Pow(altura, 2) + (Math.Pow(largura, 2)));
+            return diagonal;
+        }
+    }
+}
--- RetanguloSemClasse/Program.cs
+++ RetanguloSemClasse/Program.cs
@@ -11,25 +11,30 @@ namespace RetanguloSemClasse
             double altura;
             double largura;
             double diagonal;
-            string sep = new string('.', 50);
 
+            Retangulo retangulo = new Retangulo();
+            string sep = new string('.', 50);
 
             Console.WriteLine("Sistema Calcular Retangulo");
             Console.WriteLine(sep);
 
             Console.Write("Digite a Altura do Retangulo: ");
             altura = double.Parse(Console.ReadLine());
+            retangulo.altura = altura;
 
             Console.Write("Digite a Largura do Retangulo: ");
             largura = double.Parse(Console.ReadLine());
+            retangulo.largura = largura;
 
             Console.WriteLine(sep);
 
-            area = CalcularArea(altura, largura);
+            area = retangulo.CalcularArea(retangulo.altura, retangulo.largura);
+            perimetro = retangulo.CalculaPerimetro(retangulo.altura, retangulo.largura);
+            diagonal = retangulo.CalcularDiagonal(retangulo.altura, retangulo.largura);
 
-            perimetro = CalculaPerimetro(altura, largura);
+            
 
-            diagonal = CalcularDiagonal(altura, largura);
+            Console.WriteLine(sep);
 
             Console.WriteLine($"\nO Valor da área do Retangulo é de: {area.ToString("F2")}");
             Console.WriteLine($"\nO Valor do perimetro do Retangulo é de: {perimetro.ToString("F2")}");
@@ -37,25 +42,6 @@ namespace RetanguloSemClasse
 
             Console.WriteLine(sep);
         }
-
-        public static double CalculaPerimetro(double altura, double largura)
-        {
-            double perimetro = 2 * (altura + largura);
-            return perimetro;
-
-        }
-
-        public static double CalcularArea(double altura, double largura)
-        {
-            double area = altura * largura;
-            return area;
-        }
-        public static double CalcularDiagonal(double altura, double largura)
-        {
-            double diagonal = Math.Sqrt(Math.Pow(altura, 2) + (Math.Pow(largura, 2)));
-            return diagonal;
-        }
     }
 }
-    
 
